1debfb2c3cbf1f5ae98d9d5c47c0a1d3c19bd3ef
marcus-rubcube <marcus.vinicius@rubcube.com>
2023-11-19 22:24:25 -0300

    feat: updanting response on post category

--- controller/category-controller.js
+++ controller/category-controller.js
@@ -14,6 +14,7 @@ export default class CategoryController {
               res.status(200).send({
                 status: true,
                 message: "Categoria cadastrada!",
+                categoria: category,
               });
             })
             .catch((e) => {
